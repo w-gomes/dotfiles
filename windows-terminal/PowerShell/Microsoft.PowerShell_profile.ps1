3d4c0edc9cbc96c885cf5430b20b2b9fc1be1445
w-gomes <w.gomes5521250@gmail.com>
2025-12-11 13:30:50 -0300

    updated alias for powershell

--- windows-terminal/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ Remove-Item Alias:ls -Force
 function ls { eza --grid --color=always --group-directories-first $args }
 function ll { eza -l --grid --color=always --group-directories-first $args }
 function la { eza -al --grid --color=always --group-directories-first $args }
-function lt { eza -TL 3 --color=always --group-directories-first $args }
+function lt { eza -aTL 3 --color=always --group-directories-first $args }
+function lT { eza -aT --color=always --group-directories-first $args }
 
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
