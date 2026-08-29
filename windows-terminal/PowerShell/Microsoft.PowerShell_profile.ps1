095e5b3a94d0a6d48b8644fd29caacf01b60ebe4
w-gomes <w.gomes5521250@gmail.com>
2026-08-29 19:59:37 -0300

    windows terminal: removed jj completion

--- windows-terminal/PowerShell/Microsoft.PowerShell_profile.ps1
+++ windows-terminal/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,4 @@ function la { eza -al --grid --color=always --group-directories-first $args }
 function lt { eza -aTL 3 --color=always --group-directories-first $args }
 function lT { eza -aT --color=always --group-directories-first $args }
 
-jj util completion power-shell | Out-String | Invoke-Expression
-
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
